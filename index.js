3e3a00eaafd2b233bdeb978c7eaf11c3fdc22a47
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-06-10 12:35:24 +0600

    user workflow, Allow only job seekers to apply for jobs, Job seeker apply for jobs, Limit job seekers' number of applications for free accounts, Create pricing plan page, Create Stripe account and setup Stripe checkout session, Setup Stripe subscription plan and integrate subscription with Pro plan, Complete transaction for different types of plans for recruiter and job seeker, Get plan info from database, Update plan info after subscription

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-const express = require('express');
-const cors=require('cors');
+const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 5000
 require('dotenv').config();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 
-const uri = process.env.MONGO_DB_URI
+
+
+const uri = process.env.MONGO_DB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const database = client.db("hireloop_db");
-    const jobCollection = database.collection("jobs");
-    const companyCollection=database.collection("companies");
-    const usersCollection = database.collection("user");
 
-      app.get('/api/users', async (req, res) => {
-            
+        const database = client.db("hireloop_db");
+        const jobCollection = database.collection("jobs");
+        const companyCollection = database.collection("companies");
+        const usersCollection = database.collection("user");
+        const applicationsCollection = database.collection("applications");
+        const planCollection = database.collection('plans');
+        const subscriptionCollection = database.collection('subscriptions');
+
+        app.get('/api/users', async (req, res) => {
+
             const cursor = usersCollection.find().skip(6);
             const result = await cursor.toArray();
             res.send(result);
         })
-    
 
-     app.get('/api/jobs', async(req, res) =>{
+        app.get('/api/jobs', async (req, res) => {
             const query = {};
-            if(req.query.companyId){
+            if (req.query.companyId) {
                 query.companyId = req.query.companyId;
             }
-            if(req.query.status){
+            if (req.query.status) {
                 query.status = req.query.status;
             }
             const cursor = jobCollection.find(query);
@@ -56,7 +61,7 @@ async function run() {
             res.send(result);
         })
 
-          app.get('/api/jobs/:id', async (req, res) => {
+        app.get('/api/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
                 _id: new ObjectId(id)
@@ -65,69 +70,115 @@ async function run() {
             res.send(result);
         })
 
-    app.post('/api/jobs',async(req,res)=>{
-        const job=req.body;
-        const newJob={
-          ...job,
-          createAt:new Date()
-        }
-        const result=await jobCollection.insertOne(newJob);
-        res.send(result);
+        app.post('/api/jobs', async (req, res) => {
+            const job = req.body;
+            const newJob = {
+                ...job,
+                createdAt: new Date()
+            }
+            const result = await jobCollection.insertOne(newJob);
+            res.send(result);
+        })
 
-    })
+        // application related apis
+        app.get('/api/applications', async (req, res) => {
+            const query = {};
+            if (req.query.applicantId) {
+                query.applicantId = req.query.applicantId;
+            }
+            if (req.query.jobId) {
+                query.jobId = req.query.jobId;
+            }
+            const cursor = applicationsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+        app.post('/api/applications', async (req, res) => {
+            const application = req.body;
+            const newApplication = {
+                ...application,
+                createdAt: new Date()
+            }
+            const result = await applicationsCollection.insertOne(newApplication);
+            res.send(result);
+        })
 
-   //company related apis 
-    app.get('/api/companies', async (req, res) => {
+        // company related apis
+        app.get('/api/companies', async (req, res) => {
             const cursor = companyCollection.find().skip(4);
             const result = await cursor.toArray();
             res.send(result);
         })
 
 
+        app.get('/api/my/companies', async (req, res) => {
+            const query = {};
+            if (req.query.recruiterId) {
+                query.recruiterId = req.query.recruiterId;
+            }
+            const result = await companyCollection.findOne(query);
 
-   app.get('/api/my/companies',async(req,res)=>{
-    console.log('companies');
+            res.send(result || {});
+        })
 
-        const query={};
-        if(req.query.recruiterId){
-          query.recruiterId=req.query.recruiterId;
-          
+        app.post('/api/companies', async (req, res) => {
+            const company = req.body;
+            const newCompany = {
+                ...company,
+                createdAt: new Date()
+            }
+            const result = await companyCollection.insertOne(newCompany);
+            res.send(result);
+        })
 
-        }
-        console.log(req.query);
+        // plans 
+        app.get('/api/plans', async (req, res) => {
+            const query = {}
+            if (req.query.plan_id) {
+                query.id = req.query.plan_id
+            }
+            const plan = await planCollection.findOne(query);
+            res.send(plan)
+        })
 
-        const result=await companyCollection.findOne(query);
+        // subscription 
+        app.post('/api/subscriptions', async (req, res) => {
+            const data = req.body;
+            const subsInfo = {
+                ...data,
+                createdAt: new Date()
+            }
 
-        console.log("My companies",result);
-        res.send(result);
+            const result = await subscriptionCollection.insertOne(subsInfo);
 
-   })
+            // update the user plan information
+            const filter = { email: data.email };
+            // update the value of the 'quantity' field to 5
+            const updateDocument = {
+                $set: {
+                    plan: data.planId,
+                },
+            };
 
+            const updateResult = await usersCollection.updateOne(filter, updateDocument);
+            res.send(updateResult)
+        })
 
-   app.post('/api/companies',async(req,res)=>{
-    const company=req.body;
-    const newCompany={
-      ...company,
-      createAt:new Date()
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-    const result=await companyCollection.insertOne(company);
-    res.send(result);
-   })
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
 }
 run().catch(console.dir);
 
 
 
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
